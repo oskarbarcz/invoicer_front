0298b5430b6716082ddd0fd8216df90fbb2b4fde
Oskar Barcz <kontakt@archi-tektur.pl>
2022-02-02 21:53:58 +0100

    Add document title section

--- src/components/DocumentTitle/index.js
+++ src/components/DocumentTitle/index.js
@@ -1,7 +1,15 @@
 import './style.sass'
 
 function DocumentTitle ({invoiceNumber}) {
-    return (<div className="document-title">Invoice no. {invoiceNumber}</div>)
+    return (
+        <div className="document-title">
+            <h1 className="">
+                <span className="document-title__text">Faktura VAT nr </span>
+                <span className="document-title__text document-title__text--bold">{invoiceNumber}</span>
+            </h1>
+
+        </div>
+    );
 }
 
 export default DocumentTitle;
