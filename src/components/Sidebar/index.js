4cee7246022008ff591d763f68043d7874721a45
Oskar Barcz <kontakt@archi-tektur.pl>
2022-02-20 21:20:27 +0100

    Add hideable sidebars

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,16 @@
 import './style.sass'
+import SidebarSection from "../SidebarSection";
+
 
 function Sidebar() {
+
   return (
     <div className="sidebar">
       <h1 className="sidebar__title">Invoicer</h1>
+      <SidebarSection/>
+      <SidebarSection/>
+      <SidebarSection/>
+      <SidebarSection/>
     </div>
   );
 }
--- src/components/SidebarSection/index.js
+++ src/components/SidebarSection/index.js
@@ -1,9 +1,37 @@
 import './style.sass'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import {useState} from "react";
+
 function SidebarSection() {
+  const [expanded, setExpanded] = useState(true);
+
+  const onHideOrShow = function (){
+    setExpanded(!expanded);
+  }
+  let body = null;
+
+  if(expanded){
+    body = (
+      <div className="sidebar_section__body">
+      Section body
+    </div>
+    );
+  }
+
   return (
-    <div className="sidebar">
-      <h1 className="sidebar__title">Invoicer</h1>
+    <div className="sidebar_section">
+      <button className="sidebar_section__header" onClick={onHideOrShow}>
+        <span className="sidebar_section__header-icon">
+          <FontAwesomeIcon icon={faCoffee} fixedWidth/>
+        </span>
+        <h2 className="sidebar_section__title">Some name</h2>
+        <span className="sidebar_section__header-click">
+          <FontAwesomeIcon icon={faChevronDown} fixedWidth/>
+        </span>
+      </button>
+      {body}
     </div>
   );
 }
